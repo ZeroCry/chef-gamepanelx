9428035714f2670855348d5130fce34fef99429e
Ryan Gehrig <rdgehrig@gmail.com>
2016-08-26 12:43:12 -0700

    Cleanup

--- metadata.rb
+++
@@ -1,7 +0,0 @@
-name             'gamepanelx'
-maintainer       'GamePanelX'
-maintainer_email 'support@gamepanelx.com'
-license          'GNU GPL V2'
-description      'Installs/Configures gamepanelx'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.0'
